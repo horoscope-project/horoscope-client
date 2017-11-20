b93fce68558f18e5eb82c279a1fccef0f23c215a
Catherine Looper <loopercat@gmail.com>
2017-11-19 20:50:11 -0800

    Added toggle effect to content

--- scripts/view/horoscope-view.js
+++ scripts/view/horoscope-view.js
@@ -1,1 +1,40 @@
 'use strict';
+
+var app = app || {};
+
+((module) => {
+  var horoscopeView = {};
+
+  horoscopeView.initIndexPage = () => {
+    $('#catherine-horoscope').hide();
+    $('#sarah-horoscope').hide();
+    $('#olivia-horoscope').hide();
+    $('#jamie-horoscope').hide();
+  }
+
+  $('#catherine').on('click', () => {
+    $( '#catherine-horoscope' ).toggle( 'slow', () => {
+    });
+  });
+
+  $('#sarah').on('click',() => {
+    $( '#sarah-horoscope' ).toggle( 'slow', () => {
+    });
+  });
+
+  $('#olivia').on('click', () => {
+    $( '#olivia-horoscope' ).toggle( 'slow', () => {
+    });
+  });
+
+  $('#jamie').on('click', () => {
+    $( '#jamie-horoscope' ).toggle( 'slow', () => {
+    });
+  });
+
+  module.horoscopeView = horoscopeView;
+})(app);
+
+$(document).ready(function() {
+  app.horoscopeView.initIndexPage();
+})
